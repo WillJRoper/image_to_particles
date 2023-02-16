4c146140aa2220d4bb490847a6436a660404868e
wjr21 <wjr21@sussex.ac.uk>
2023-02-16 18:55:03 +0000

    Debugging

--- makeMovieSwiftsimIO_color.py
+++ makeMovieSwiftsimIO_color.py
@@ -106,6 +106,7 @@ if __name__ == "__main__":
         # Loop over colors
         for icol in range(3):
 
+            print(data.gas.masses)
             data.gas.masses = rgbs[pids, icol] * unyt.Msun
 
             mesh[:, :, icol] = project_gas_pixel_grid(
