c7aab93e6c3a2e7aa6e7627b56d20cc123c79f13
wjr21 <wjr21@sussex.ac.uk>
2023-02-16 22:43:54 +0000

    Debugging

--- makeMovieSwiftsimIO_color.py
+++ makeMovieSwiftsimIO_color.py
@@ -134,9 +134,8 @@ if __name__ == "__main__":
         for icol in range(3):
 
             data = load_and_extract(fn)
-            pids = data.gas.particle_ids
-            print(n, icol, pids)
-            data.gas.masses[:] = rgbs[pids, icol]
+            print(n, icol, pids, np.all(pids[:-1] <= pids[1:]))
+            data.gas.masses[:] = rgbs[:, icol]
 
             mesh[:, :, icol] = project_gas_pixel_grid(data, dpi)
 
